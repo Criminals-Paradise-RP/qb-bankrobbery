395defc1a42aece27a339ac837ac2926a5f5ce85
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-07-29 18:21:45 -0700

    Update cl_config.lua

--- cl_config.lua
+++ cl_config.lua
@@ -62,8 +62,8 @@ function Config.ShowRequiredItems(items, show)
     TriggerEvent('inventory:client:requiredItems', items, show)
 end
 
-Config.MinimumPaletoPolice = 0
-Config.MinimumPacificPolice = 0
-Config.MinimumFleecaPolice = 0
-Config.MinimumThermitePolice = 0
+Config.MinimumPaletoPolice = 4
+Config.MinimumPacificPolice = 5
+Config.MinimumFleecaPolice = 3
+Config.MinimumThermitePolice = 2
 Config.OutlawCooldown = 5 -- The amount of minutes it takes for the cops to be able to be called again after they were called
